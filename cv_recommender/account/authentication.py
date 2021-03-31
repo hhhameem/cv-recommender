68051840da89473af57ba64afc9068b4bc0f3695
hhhameem <hebronhamim@gmail.com>
2021-03-31 13:00:47 +0600

    fix email authentication problem

--- cv_recommender/account/authentication.py
+++ cv_recommender/account/authentication.py
@@ -1,14 +1,18 @@
 from django.contrib.auth.models import User
 
 
-class EmailAuthentication(object):
+class EmailAuthBackend(object):
+    """
+    Authenticate using an e-mail address.
+    """
+
     def authenticate(self, request, username=None, password=None):
         try:
             user = User.objects.get(email=username)
             if user.check_password(password):
                 return user
             return None
-        except user.DoesNotExist:
+        except User.DoesNotExist:
             return None
 
     def get_user(self, user_id):
--- cv_recommender/cv_recommender/settings.py
+++ cv_recommender/cv_recommender/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
-    'account.authentication.EmailAuthentication',
+    'account.authentication.EmailAuthBackend',
 ]
 
 # Internationalization
