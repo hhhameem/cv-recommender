c52d82dc170a057c7e547124f4ef08d7368af2da
hhhameem <hebronhamim@gmail.com>
2021-07-24 10:28:04 +0600

    Little bit change in the url

--- cv_recommender/cv_recommender/urls.py
+++ cv_recommender/cv_recommender/urls.py
@@ -21,8 +21,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path('', include('cvrecommender.urls')),  # cvrecommender/
     path('account/', include('account.urls')),
-    path('cvrecommender/', include('cvrecommender.urls')),
     path('admin/', admin.site.urls),
 ]
 
